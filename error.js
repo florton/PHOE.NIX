5267b9a45198e8e85cc038a4d593b442dfaad7df
Flanders Lorton <florton@lion.lmu.edu>
2015-02-10 11:59:05 -0800

    PHOE.NIX

--- error.js
+++ error.js
@@ -2,7 +2,7 @@ module.exports = {
 error : function(line,line_Num,line_Pos){
 var pos = "^";
 for (var x=0; x<line_Pos; x++){pos = "-"+pos;}
-var alert = "Syntax Error at: Line: " + line_Num + " Char: " + line_Pos;
+var alert = "Lexical Error at: Line: " + line_Num + " Char: " + line_Pos;
 var error = alert + "\n" + line + "\n" + pos;
 throw error;
 }
