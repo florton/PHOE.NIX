fb9d220373b122c859387d5a1a56c745c7c58917
dannymlevine <danlevine@Dans-MacBook-Pro.local>
2015-02-22 23:39:15 -0800

    more framework for parser
    some initial work done on the parser

--- PhoenixParser.js
+++ PhoenixParser.js
@@ -10,11 +10,11 @@ scanner("photest.txt", function (tokens) {
         do {
             statements.push(parseStatement())
             match('EOL')
-        } while (at([ 'type', 'ID', 'while', 'if', 'else', 'for', 'do', 'class', 'access']))
+        } while (at([ 'int', 'bool', 'string', 'double', 'void', 'func', 'ID', 'while', 'if', 'else', 'for', 'do', 'class', 'public', 'private']))
         return new Block(statements)
     }
     function parseStatement(){
-        if (at ('int' || 'bool' || 'string' || 'double' || 'void' || 'func') ){
+        if (at ('int' || 'bool' || 'string' || 'double' || 'void') ){
         	return parseType();
         } else if ('ID'){
         	return parseAssignmentStatement();
@@ -32,7 +32,86 @@ scanner("photest.txt", function (tokens) {
         	return parseMemberDeclaration();
         } else if ('else'){
         	return parseElseStatement();
+        } else if ('func'){
+        	return parseLambdaFunction();
+        } else{
+        	error('no Statement', tokens[0])
         }
+    }
+
+    function parseType(){
+
+    }
+
+    function parseAssignmentStatement(){
+
+    }
+
+    function parseWhileStatement(){
+    	// match('while');
+     //    var condition = parseExpression();
+     //    var body = parseBlock();
+     //    return new WhileStatement(condition, body);
+    }
+
+    function parseIfStatement(){
+
+    }
+
+    function parseForStatement(){
+
+    }
+
+    function parseDoStatement(){
+
+    }
+
+    function parseClassDeclaration(){
+
+    }
+
+    function parseElseStatement(){
+
+    }
+
+    function parseMemberDeclaration(){
+
+    }
+
+    function parseLambdaFunction(){
+
+    }
+
+    function parseExp1(){
+
+    }
+
+    function parseExp2(){
+
+    }
+
+    function parseExp3(){
+
+    }
+
+    function parseExp4(){
+
+    }
+
+    function parseExp5(){
+
+    }
+
+    function parseExp6(){
+
+    }
+
+    function parseExp7(){
+
+    }
+
+    function parseExp8(){
+
     }
 
     function at(kind) {
@@ -42,39 +121,16 @@ scanner("photest.txt", function (tokens) {
             return type.some(at)
         } else {
             return type === tokens[0].type
-     }  
-}
+        }  
+    }
 
-
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   function match(kind) {
+        if (tokens.length === 0) {
+            error('Unexpected end of source program')
+        } else if (type === undefined || type === tokens[0].type) {
+            return tokens.shift()
+        } else {
+            error('Expected ' + type + ' but found ' + tokens[0].type, tokens[0])
+        }
+    }
 });
