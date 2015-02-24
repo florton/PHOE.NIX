91abd4b2d5936682a31e5c4533ed238bd620ba8a
mleavell <mauriceleavell@gmail.com>
2015-02-23 18:48:47 -0800

    Spelling Error fixes

--- parserMockup2.js
+++ parserMockup2.js
@@ -1,35 +1,35 @@
 var scanner = require("./scanner.js").scan;
-
+ 
 scanner("photest.txt", function (tokens) {
     //main
     var tokenIndex = 0;
     var indents = [0, 0];
-
-    while (tokenIndex < token.length - 1) {
+ 
+    while (tokenIndex < tokens.length - 1) {
         parseScript();
     }
-
+ 
     function parseScript() {
-        indentlevel();
+        indentLevel();
         if (!parseStatement()) {
             //error
         }
         //success?
     }
-
+ 
     function indentLevel() {
         while (at('indent')) {
             indents[1]++;
         }
     }
-
+ 
     function parseEnd() {
         if (at('EOL')) {
             indents = [indents[1], 0];
             return true;
         }
     }
-
+ 
     function at(type) {
         while (tokens[tokenIndex].type === 'comment' || (tokens[tokenIndex].type === 'indent' && type !== 'indent')) {
             tokenIndex++;
@@ -41,7 +41,7 @@ scanner("photest.txt", function (tokens) {
             return false;
         }
     }
-
+ 
     function parseBlock() {
         while (indents[1] >= indents[0]) {
             if (parseScript()) {
@@ -51,21 +51,31 @@ scanner("photest.txt", function (tokens) {
             }
         }
     }
-
+ 
     function parseStatement() {
         if (at('class')) {
-            if (parseClass()) {}
-        }
-        if (at('type')) {
+            return parseClassDec()
+        } else if (at('type')) {
             return parseType();
+        }else if ('id'){
+            return parseAssignmentStatement();
+        } else if ('while'){
+            return parseWhileStatement();
+        } else if ('if'){
+            return parseIfStatement();
+        } else if ('for'){
+            return parseForStatement();
+        } else if ('do'){
+            return parseDoStatement();
+        } else if ('public' || 'private'){
+            return parseMemberDeclaration();
+        } else if ('else'){
+            return parseElseStatement();
+        } else{
+            error('no Statement', tokens[0])
         }
-        if (at('id')) {}
-        if (at('for')) {}
-        if (at('do')) {}
-        if (at('while')) {}
-        if (at('if')) {}
     }
-
+ 
     function parseClassDec() {
         if (at('id')) {
             if (parseEnd()) {
@@ -76,27 +86,31 @@ scanner("photest.txt", function (tokens) {
         }
         return false;
     }
-
+ 
     function parseType() {
         if (at('id')) {
             if (at('paren')) {
-                return parseParens(token[tokenIndex - 1].lexeme);
+                return parseParens(tokens[tokenIndex - 1].lexeme);
             } else {
                 return //parseAssmt();
-            }   
+            }  
         }
         return false;
     }
-
+ 
     function parseParens(paren) {
-        if (paren === "(") { 
+        if (paren === "(") {
             //function
             //parseParams()
         }
         if (paren === "[") {
             //array
             //exp4 or exp1 ?
-        }   
+        }  
     }
-    
+ 
+    function parseAssignmentStatement(){
+       
+    }
+   
 });
