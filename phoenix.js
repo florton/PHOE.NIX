4ea1ef341e1538553b2b7b8fb64f38bd30fd8f25
dannymlevine <danlevine@Dans-MacBook-Pro.local>
2015-03-24 12:53:31 -0700

    made small program to run parser

--- phoenix.js
+++ phoenix.js
@@ -1,8 +1,18 @@
+var argv = require('yargs')
+  .usage('$0 [-t] [-a]')
+  .boolean(['t','a'])
+  .describe('t', 'show tokens after scanning then stop')
+  .describe('a', 'show abstract syntax tree after parsing then stop')
+  .demand(1)
+  .argv
+
 var scan = require('./scanner')
 var parse = require('./parser').parse
-var error = require('./error') 
-parse(process.argv[2], function (tokens) { 
-    var program = tokens 
+var error = require('./error')
+
+parse(argv._[0], function (tokens) {
+  var program = tokens
     console.log(program.toString())
-    return //program.analyze() 
+    return
+  //program.analyze()
 })
