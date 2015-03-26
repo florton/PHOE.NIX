e2fb84ffc06f4de67d7ac813beeebc3b40205594
Flanders Lorton <florton@lion.lmu.edu>
2015-03-25 21:46:18 -0700

    test parser working + tests added

--- test/test-parser.js
+++ test/test-parser.js
@@ -2,36 +2,69 @@ var should = require('should');
 var parser = require("../parser.js").parse;
 var i = require('util').inspect;
 
-var tests = 0;
-
-parser('data/hello.nix', function (bool) {
-    if(bool) console.log("hello.nix passed");
+describe('Hello World', function(){
+    it('should save without error', function(done){
+      parser('test/data/hello.nix', function(stuff) {done();});
+    });
 });
 
-parser('data/for.nix', function (bool) {
-    if(bool) console.log("for.nix passed");
+describe('Variable Declarations', function(){
+    it('should save without error', function(done){
+      parser('test/data/variables.nix', function(stuff) {done();});
+    });
 });
 
-parser('data/variables.nix', function (bool) {
-    if(bool) console.log("variables.nix passed");
+describe('For Loop', function(){
+    it('should save without error', function(done){
+      parser('test/data/for.nix', function(stuff) {done();});
+    });
 });
 
-parser('data/while.nix', function (bool) {
-    if(bool) console.log("while.nix passed");
+describe('While Loop', function(){
+    it('should save without error', function(done){
+      parser('test/data/while.nix', function(stuff) {done();});
+    });
 });
 
-parser('data/broken/hello.nix', function (bool) {
-    if(!bool) console.log("broken version hello.nix failed");
+describe('Arrays', function(){
+    it('should save without error', function(done){
+      parser('test/data/arrays.nix', function(stuff) {done();});
+    });
 });
 
-parser('data/broken/for.nix', function (bool) {
-    if(!bool) console.log("broken version for.nix failed");
+describe('Functions', function(){
+    it('should save without error', function(done){
+      parser('test/data/function.nix', function(stuff) {done();});
+    });
 });
 
-parser('data/broken/variables.nix', function (bool) {
-    if(!bool) console.log("broken version variables.nix failed");
+describe('Member Declarations', function(){
+    it('should save without error', function(done){
+      parser('test/data/public.nix', function(stuff) {done();});
+    });
 });
 
-parser('data/broken/while.nix', function (bool) {
-    if(!bool) console.log("broken version while.nix failed");
+describe('Broken Hello World', function(){
+    it('should throw error', function(done){
+      parser('test/data/broken/hello.nix', done());
+    });
 });
+
+describe('Broken Variable Declarations', function(){
+    it('should throw error', function(done){
+      parser('test/data/broken/variables.nix', done());
+    });
+});
+
+describe('Broken For Loop', function(){
+    it('should throw error', function(done){
+      parser('test/data/broken/for.nix', done());
+    });
+});
+
+describe('Broken While Loop', function(){
+    it('should throw error', function(done){
+      parser('test/data/broken/while.nix', done());
+    });
+});
+
