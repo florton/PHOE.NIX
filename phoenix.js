d55c685f4e848338ef6b13bd3a053fa0eebd4cb2
Flanders Lorton <florton@lion.lmu.edu>
2015-04-11 15:56:57 -0700

    phoenix.js works with command line options

--- phoenix.js
+++ phoenix.js
@@ -1,18 +1,32 @@
 var argv = require('yargs')
-  .usage('$0 [-t] [-a]')
-  .boolean(['t','a'])
+  .usage('$0 filename [-t] [-a] [-o] [-i]')
+  .boolean(['t','a','o','i'])
   .describe('t', 'show tokens after scanning then stop')
   .describe('a', 'show abstract syntax tree after parsing then stop')
+  .describe('o', 'do optimizations')
+  .describe('i', 'generate and show the intermediate code then stop')
   .demand(1)
   .argv
 
-var scan = require('./scanner')
+
+//var generate = require('./generator')(argv.target)
+var scan = require('./scanner').scan
 var parse = require('./parser').parse
 var error = require('./error')
 
+scan(argv._[0], function (tokens) {
+  if(argv.t){
+    var program = tokens
+    console.log(tokens)
+    return
+  }
+})
+
 parse(argv._[0], function (tokens) {
-  var program = tokens
+  if(argv.a){
+    var program = tokens
     console.log(program.toString())
     return
+  }
   //program.analyze()
 })
