4c82337edb48df5226dcf7752d5ff42db794daee
dannymlevine <danlevine@Dans-MacBook-Pro.local>
2015-04-18 14:38:28 -0700

    made analyze for if
    made analyze function for if entity

--- entities/IfStatement.js
+++ entities/IfStatement.js
@@ -7,4 +7,10 @@ ifStatement.prototype.toString = function () {
     return "(if: " + this.condition.toString() + "\n" + this.block.toString() +")"
 }
 
+ifStatement.prototype.analyze = function(context){
+	this.condition.analyze(context)
+	this.condition.type.mustBeBoolean('Condition in "if" statement must be boolean')
+    this.body.analyze(context)
+}
+
 module.exports = ifStatement
