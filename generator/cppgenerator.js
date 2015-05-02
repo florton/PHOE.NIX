72dad5c3ab6012b637d0dbf6a7570fbfb870dafa
dannymlevine <danlevine@Dans-MacBook-Pro.local>
2015-05-02 01:34:42 -0700

    added inline optimization
    made functions inline

--- generator/cppgenerator.js
+++ generator/cppgenerator.js
@@ -153,7 +153,7 @@ var generator = {
             }
         }
         inFunc = true
-        emit(func.type + " " + gen(func.name) + '(' + params + ')' +'{')
+        emit("inline"+func.type + " " + gen(func.name) + '(' + params + ')' +'{')
         gen(func.block)
         emit('}')
         inFunc = false
