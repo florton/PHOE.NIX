3b567572db18e80ef92eebdbc6c8e6d83837a2c7
Flanders Lorton <florton@lion.lmu.edu>
2015-05-01 21:28:59 -0700

    broken code

--- analyzer.js
+++ analyzer.js
@@ -1,5 +1,7 @@
 var error = require('./error.js').analyzeError
 var VariableDeclaration = require('./entities/variableDeclaration.js')
+var variableReference = require("./entities/variableReference.js")
+var attribute = require("./entities/attribute.js")
 
 function AnalysisContext(parent) {
   this.parent = parent
@@ -25,14 +27,20 @@ AnalysisContext.prototype.addVariable = function (name, entity) {
 }
 
 AnalysisContext.prototype.lookupVariable = function (token) {
-  var variable = this.symbolTable[token]
-  if (variable) {
-    return variable
-  } else if (!this.parent) {
-    error('Variable ' + token + ' not found')
-    return varDec.ARBITRARY
-  } else {
-    return this.parent.lookupVariable(token)
+  if(token instanceof attribute){
+    token.left = this.lookupVariable(token.left)
+    token.right = this.lookupVariable(token.right) 
+    return token
+  }else{
+    var variable = this.symbolTable[token]
+    if (variable) {
+        return variable
+    } else if (!this.parent) {
+        if(token instanceof variableReference){token = token.token}
+        error('Variable ' + token + ' not found')
+    } else {
+        return this.parent.lookupVariable(token)
+    }
   }
 }
 
---
+++ test.cpp
@@ -0,0 +1,27 @@
+arr
+st
+st
+st
+userIn
+userIn
+{ token: 'nextOdd' }
+{ token: 'nextOdd' }
+{ token: 'nextOdd' }
+{ token: 'nextOdd' }
+{ token: 'nextOdd' }
+{ token: 'nextOdd' }
+{ token: 'nextOdd' }
+{ token: 'nextOdd' }
+x
+x
+x
+x
+x
+x
+x
+x
+x
+{ token: 'fibonacci' }
+{ token: 'nextOdd' }
+{ left: { token: 'object' }, right: { token: 'input_value' } }
+{ left: { token: 'object' }, right: { token: 'input_value' } }
